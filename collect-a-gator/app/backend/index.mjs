982f5366bcfa32ed43284b3277950be1a22d4d32
ealfla8 <68116219+ealfla8@users.noreply.github.com>
2025-04-20 21:14:36 -0400

    changed url and fixed cors issues

--- collect-a-gator/app/backend/index.mjs
+++ collect-a-gator/app/backend/index.mjs
@@ -8,7 +8,11 @@ import userRoutes from "../api/user/route.mjs";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
+app.use(cors({                                                
+  origin: ['http://74.249.99.214', 'http://74.249.99.214:80'],
+  credentials: true                                           
+}));                                                          
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
--- collect-a-gator/next.config.ts
+++ collect-a-gator/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    allowedDevOrigins: ['http://74.249.99.214']
+  }
 };
 
 export default nextConfig;
